56340cd2ee0461ae7f5b51df0687a2e1253cf4b5
Goneri Le Bouder <goneri@rulezlan.org>
2006-02-15 17:41:13 +0000

    Document.pm moved from lib to lib/QSOS GPL header added

--- libs/perl/QSOS-Document/lib/QSOS/Document.pm
+++ libs/perl/QSOS-Document/lib/QSOS/Document.pm
@@ -1,3 +1,24 @@
+# $Id: Document.pm,v 1.1 2006/02/15 17:41:13 goneri Exp $
+#
+#  Copyright (C) 2006 Atos Origin 
+#
+#  Author: Gonéri Le Bouder <goneri.lebouder@atosorigin.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
 package QSOS::Document;
 use XML::Twig;
 use Carp;
