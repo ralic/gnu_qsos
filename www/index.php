2ad03e787e5aef8edb0e5df1d9135524b4130f01
Goneri Le Bouder <goneri@rulezlan.org>
2006-03-10 16:51:48 +0000

    new latex file "overview.tex". It give an overview of QSOS. It's the first page of qsos.org. Need to be translated.

--- www/index.php
+++ www/index.php
@@ -2,30 +2,9 @@
 <?
 $lang = getlang();
 head($lang);
-?>
 
 
-
-<h1>Présentation de QSOS</h1>
-<p>
- QSOS est une méthode conçue pour qualifier, sélectionner et comparer les logiciels open source. Elle est mise à disposition de la communauté sous licence libre GNU Free Documentation License.
-</p>
-<img alt="présentation du processus" src="images/processus.png"/>
-<p>
-QSOS consiste en un processus itératif en quatre étapes :
-</p>
-<ul>
-<li><strong>Définir</strong> les données de référentiel (types de licences, types de communautés, grilles de couverture fonctionnelle par domaine, …)</li>
-<li><strong>Évaluer</strong> les logiciels selon trois axes principaux : couverture fonctionnelle, risques du point de vue de l’entreprise utilisatrice, risques du point de vue du fournisseur de services (expertise, formation, support).
- Chaque axe est constitué d’un certains nombre de critères. Par exemple, l’axe des risques entreprise comprend : la pérennité intrinsèque, l’intégration, l’adaptabilité technique, le niveau d’industrialisation et la stratégie du projet. Ces critères étant eux-même composés de sous-critères.</li>
-<li><strong>Qualifier</strong> le contexte spécifique d’une entreprise (ou d’un utilisateur) en effectuant une pondération des critères précédents.</li>
-<li><strong>Sélectionner</strong> et comparer les logiciels répondant aux besoins.</li>
-</ul>
+include("methode/$lang/overview.html");
 
 
-<p>
-Ce processus génère des fiches d’identités de logiciel ainsi que des grilles de comparaison et de choix. La démarche par étape, les multiples critères d'analyse, et la métrologie définis par QSOS en font une méthode qui permet une évaluation objective et argumentée des logiciels libres – précieuse notamment dans des phases amont d'étude d'opportunité de migration vers les logiciels libres ainsi que pour choisir une solution open source optimale dans un contexte donné. 
-</p>
-
-
-<?php foot();?>
+foot();?>
