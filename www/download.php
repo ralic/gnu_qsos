a25bb7fb9dfe604a3933778ab271923ab28c70b7
Goneri Le Bouder <goneri@rulezlan.org>
2006-03-28 20:18:02 +0000

    i18n

--- www/download.php
+++ www/download.php
@@ -4,31 +4,25 @@ $lang = getlang();
 head($lang);
 ?>
 
-<h1>Téléchargement</h1>
-
-<h2>Méthode QSOS version 1.5</h2>
+<h2>QSOS version 1.5</h2>
 <div class="downloads">
-<ul>
-<li class="pdf"><a href="download/qsos-1.5.pdf">Format PDF</a></li>
-<li class="dvi"><a href="download/qsos-1.5.dvi">Format DVI</a></li>
-<li class="tgz"><a href="download/qsos-1.5.tar.gz">Latex</a></li>
+English
+<ul lang="en">
+<li class="pdf"><a href="download/en/qsos-1.5.pdf">PDF</a></li>
+<li class="dvi"><a href="download/en/qsos-1.5.dvi">DVI</a></li>
 </ul>
-</div>
-
-<h2>Méthode QSOS version 1.4</h2>
-<div class="downloads">
-<ul>
-<li class="odt"><a href="download/MQSOS_V1.4.odt">Format OpenOffice.org 2 (source)</a></li>
-<li class="pdf"><a href="download/MQSOS_V1.4.pdf">Format PDF</a></li>
+French
+<ul lang="fr">
+<li class="pdf"><a href="download/fr/qsos-1.5.pdf">PDF</a></li>
+<li class="dvi"><a href="download/fr/qsos-1.5.dvi">DVI</a></li>
 </ul>
+source
+<ul>
+<li class="tgz"><a href="download/fr/qsos-1.5.tar.gz">Latex</a></li>
+</ul>
+
 </div>
 
-<h2>Modèle de fiche QSOS</h2>
-<div class="downloads">
-<ul>
-<li class="ods"><a href="download/Modele_Fiche_QSOS v1.1.ods">Modèle de fiche QSOS v1.1</a></li>
-</li>
-</div>
 
 <h2>Gnu FDL</h2>
 <div class="downloads">
