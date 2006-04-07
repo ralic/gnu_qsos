020dd3223487a855cf503038b4743012254be007
Arthur Clerfeuille <arthur.clerfeuille@atosorigin.com>
2006-04-07 13:07:48 +0000

    *** empty log message ***

---
+++ libs/java/QSOS-Document/src/org/qsos/utils/ILibQSOS.java
@@ -0,0 +1,262 @@
+/* $Id: ILibQSOS.java,v 1.1 2006/04/07 13:09:36 aclerf Exp $
+*
+*  Copyright (C) 2006 Atos Origin 
+*
+*  Author: Arthur Clerfeuille <arthur.clerfeuille@atosorigin.com>
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package org.qsos.utils;
+
+import java.net.URL;
+
+/**This interface should be implemented by LibQSOS.
+ * It contains all the methods of the api defined to make editors.
+ * @author aclerf
+ *
+ */
+public interface ILibQSOS {
+	/**
+	 * Allows to load the java model corresponding to the xml document found at the 
+	 * url.
+	 * @param url the URL where the xml document is get.
+	 */
+	 public void load(URL url);
+	
+	/**
+	 * Allows to get the description number numDesc of the element called name.
+	 * @param name the name to search.
+	 * @param numDesc int representing the number of the description to search.
+	 * @return a String corresponding to the description asked.
+	 */
+	 public String getDescByName(String name, int numDesc);
+
+	/**
+	 * Allows to set a comment to an element given by his name.
+	 * 
+	 * @param name the name of the element.
+	 * @param comment the comment to set.
+	 */
+	 public void setCommentByName(String name,String comment);
+	
+	/**
+	 * Allows to get the comment on an element.
+	 * 
+	 * @param name the name of the element to get.
+	 * @return a String corresponding to the comment of the element asked.
+	 */
+	 public String getCommentByName(String name);
+	
+	/** Allows to get the score of an element.
+	 * 
+	 * @param name the name of the element.
+	 * @return a String representing the score of the Element.
+	 */
+	public String getScoreByName(String name);
+	
+	/**Allows to set the score of an element
+	 * 
+	 * @param name the name of the element.
+	 * @param score a String representing the score to set.
+	 */
+	public void setScoreByName(String name,String score);
+	
+	/**
+	 * Allows to get the name of all the authors.
+	 * 
+	 * @return a String that contains the names of all the authors.
+	 */
+	public String getAuthors();
+	
+	/**Allows to add an author to the list of authors.
+	 * 
+	 * @param nameString the name of the author to add.
+	 * @param emailString the email of the author to add.
+	 */
+	public void addAuthor(String nameString, String emailString) ;
+
+	/**Allows to delete an author.
+	 * 
+	 * @param name the name of the author to delete.
+	 */
+	public void delAuthor(String name);
+
+	/* All the method describes backwards allow to get or set the element inside the header */
+	
+	
+	/**Allows to get the application name.
+	 * 
+	 * @return a string corresponding to the application name.
+	 */
+	public String getAppname();
+	
+	/**Allows to set the application name.
+	 * 
+	 * 
+	 * @param appname the application name to set.
+	 */
+	public void setAppname(String appname);
+	
+	
+	/**Allows to get the language.
+	 * 
+	 * @return a string corresponding to the language.
+	 */
+	public String getLanguage();
+	
+	/**Allows to set the language.
+	 * 
+	 * 
+	 * @param language the language to set.
+	 */
+
+	public void setLanguage(String language);
+
+	/**Allows to get the release number.
+	 * 
+	 * @return a string corresponding to the release number.
+	 */
+	public String getRelease();
+	
+	/**Allows to set the release.
+	 * 
+	 * 
+	 * @param release the release to set.
+	 */
+
+	public void setRelease(String release);
+	
+	/** Not implemented yet 
+	 */
+	public String getLicenselist();
+
+	
+	/**Allows to get the license Id.
+	 * 
+	 * @return a string corresponding to the License Id.
+	 */
+	public String getLicenseId();
+	
+	/**Allows to set the license Id.
+	 * 
+	 * 
+	 * @param licenseId the license Id to set.
+	 */
+	public void setLicenseId(String licenseId);
+	
+	/**Allows to get the license Description.
+	 * 
+	 * @return a string corresponding to the license Description.
+	 */	
+	public String getLicenseDesc();
+	
+	/**Allows to set the license Description.
+	 * 
+	 * 
+	 * @param licensedesc the license Description to set.
+	 */
+
+	public void setLicenseDesc(String licensedesc);
+	
+	/**Allows to get the url.
+	 * 
+	 * @return a string corresponding to the url.
+	 */
+	public String getUrl();
+	
+	/**Allows to set the url.
+	 * 
+	 * 
+	 * @param url the url to set.
+	 */
+	public void setUrl(String url);
+	
+	
+	/**Allows to get the description.
+	 * 
+	 * @return a string corresponding to the description.
+	 */
+	public String getDesc();
+	
+	/**Allows to set the description.
+	 * 
+	 * 
+	 * @param desc the description to set.
+	 */
+	public void setDesc(String desc);
+	
+	/**Allows to get the demonstration url.
+	 * 
+	 * @return a string corresponding to the demonstration url.
+	 */
+	public String getDemoUrl();
+	
+	/**Allows to set the demonstration url.
+	 * 
+	 * 
+	 * @param demourl the demonstration url to set.
+	 */
+	public void setDemoUrl(String demourl);
+	
+	/**Allows to get the QSOS format.
+	 * 
+	 * @return a string corresponding to the QSOS format.
+	 */
+	public String getQsosformat();
+	
+	/**Allows to set the QSOS format.
+	 * 
+	 * 
+	 * @param qsosformat the QSOS format to set.
+	 */
+	public void setQsosformat(String qsosformat);
+	
+	/**Allows to get the QSOS specific format.
+	 * 
+	 * @return a string corresponding to the QSOS specific format.
+	 */
+	public String getQsosspecificformat();
+	
+	/**Allows to set the QSOS specific format.
+	 * 
+	 * 
+	 * @param qsosspecificformat the QSOS specific format to set.
+	 */
+
+	public void setQsosspecificformat(String qsosspecificformat);
+	
+	/**Allows to get the application family in QSOS.
+	 * 
+	 * @return a string corresponding to the application family in QSOS.
+	 */
+	public String getQsosappfamily();
+	
+	/**Allows to set the application family in QSOS.
+	 * 
+	 * 
+	 * @param qsosappfamily the application family in QSOS to set.
+	 */
+
+	public void setQsosappfamily(String qsosappfamily);
+
+	/**Allows to write the xml file at the given path. 
+	 * This method has a problem since it degrated the xml file (
+	 * not the datas but the presentation).
+	 * It will be fixed in the next version
+	 * 
+	 * @param path
+	 */
+	public void write(String path);
+}
---
+++ libs/java/QSOS-Document/src/org/qsos/utils/Search.java
@@ -0,0 +1,194 @@
+/* $Id: Search.java,v 1.1 2006/04/07 13:09:36 aclerf Exp $
+*
+*  Copyright (C) 2006 Atos Origin 
+*
+*  Author: Arthur Clerfeuille <arthur.clerfeuille@atosorigin.com>
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package org.qsos.utils;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.qsos.data.IElement;
+import org.qsos.data.ISheet;
+
+
+/**
+ * @author aclerf
+ *
+ */
+public class Search {
+	private ISheet sheet;
+	private String name;
+	private int numDesc;
+	private String string2set;
+	private String type;
+	
+	public void setSheet(ISheet sheet){
+		this.sheet = sheet;
+	}
+	
+	public List<IElement> searchByName() throws IOException{
+		List<IElement> list = new LinkedList<IElement>();		
+		return searchRecByName(sheet.getRoot(),list);
+	}
+	
+	
+	public List<IElement> searchRecByName(IElement target,List<IElement> list) throws IOException{
+		if (target.getElements() != null)
+			{
+				for (IElement child : target.getElements())
+					{
+						if(child.getName() != null && child.getName().equals(name)){
+							list.add(child);
+						}
+						searchRecByName(child,list);
+					}
+				return list;
+			}
+	return null;
+	}
+
+	public List<IElement> searchByMeta() throws IOException{
+		List<IElement> list = new LinkedList<IElement>();		
+		return searchRecByMeta(sheet.getRoot(),list);
+	}
+	
+	
+	public List<IElement> searchRecByMeta(IElement target,List<IElement> list){
+		if (target.getElements() != null)
+			{
+				for (IElement child : target.getElements())
+					{
+						if(child.getMeta() != null && child.getMeta().equals(name)){
+							list.add(child);
+						}
+						searchRecByMeta(child,list);
+					}
+				return list;
+			}
+		return null;
+	}
+
+	
+	
+	
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param numDesc
+	 */
+	public void setNumDesc(int numDesc) {
+		this.numDesc = numDesc;
+	}
+
+	/**
+	 * @return
+	 * @throws IOException 
+	 */
+	public String searchString() throws IOException {
+		List<IElement> list = new LinkedList<IElement>();
+		if( type.equals("name") )
+			list = searchByName();
+		else if (type.equals("meta"))
+			list = searchByMeta();
+		else throw new IOException();
+		if (list.size() > 1)
+		{
+			String res = "";
+			for (IElement member : list)
+				res = res + member.getText() + "\t";
+			return res;
+		}
+		else if(list.size() == 1) 
+			{
+				IElement temp = list.get(0);
+				if(numDesc ==-1)
+					return temp.getText();
+				else if(numDesc == 0)
+					return temp.getDesc0();
+				else if (numDesc == 1)
+					return temp.getDesc1();
+				else if (numDesc == 2)
+					return temp.getDesc2();
+				else throw new IOException();
+			}
+		else throw new IOException();
+	}
+
+	public IElement searchIElement() throws IOException{
+		List<IElement> list = new LinkedList<IElement>();
+		if( type.equals("name") )
+			list = searchByName();
+		else if (type.equals("meta"))
+			list = searchByMeta();
+		if (list.size() == 1)
+		{
+			return list.get(0);
+		}
+		else throw new IOException();
+	}
+	
+	public void searchAndSet(){
+		try {
+			searchIElement().setText(string2set);	
+		} catch (IOException e) {
+			System.err.println("Can't find Key");
+		}
+	}
+		
+	public String searchAndGetString(){
+		try {
+			return searchString();		
+		} catch (IOException e) {
+			System.err.println("Can't find Key");
+		}
+		return null;
+	}
+	
+	public IElement searchAndGetIElement(){
+		try {
+			return searchIElement();		
+		} catch (IOException e) {
+			System.err.println("Can't find Key");
+		}
+		return null;
+	}
+
+	/**
+	 * @param string2set
+	 */
+	public void setString2set(String string2set) {
+		this.string2set = string2set;
+		
+	}
+
+	/**
+	 * @param type
+	 */
+	public void setType(String type) {
+		this.type = type;
+		
+	}
+	
+}
