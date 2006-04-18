2112912e42242088dff266628de376050ba5d37f
Goneri Le Bouder <goneri@rulezlan.org>
2006-04-18 09:47:25 +0000

    print the template of a qsos file

--- tools/showtemplate/showtemplate.pl
+++ tools/showtemplate/showtemplate.pl
@@ -20,11 +20,29 @@ if (!($file && (-f $file))) {
   exit 1;
 }
 
+my $inheader;
 open FILE,"<".$file or die "can't open $file: $!";
 foreach(<FILE>) {
+
+  $inheader = 1 if (/<header>/);
+  $inheader = undef if (/<\/header>/);
+  s!(<creation>).+(<\/creation>)!$1$2!g;
+  s!(<appname>).+(<\/appname>)!$1$2!g;
+  s!(<licensedesc>).+(<\/licensedesc>)!$1$2!g;
+  s!(<url>).+(<\/url>)!$1$2!g;
+  s!(<email>).+(<\/email>)!$1$2!g;
+  s!(<name>).+(<\/name>)!$1$2!g;
+  s!(<qsosappfamily>).+(<\/qsosappfamily>)!$1$2!g;
+  s!(<demourl>).+(<\/demourl>)!$1$2!g;
+  if ($inheader) {
+    s!(<desc>).+(<\/desc>)!$1$2!g;
+  }
+
   s!(<comment>).+(<\/comment>)!$1$2!g;
   s!(<score>).+(</score>)!$1$2!g;
-  chomp if (s!^[\ ]{0,}$!!);
+  s!<\!--.+-->!!g;
+  s!>[\ \t]+$!>!g;
+  chomp if (s!^[\t\ ]{0,}$!!);
   print;
   if ($global && /<\/section>/) {
     print "<document>\n";
