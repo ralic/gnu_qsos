e79f6e9d83046fd6c506b31b452bb45734e7927e
Goneri Le Bouder <goneri@rulezlan.org>
2006-06-22 09:11:10 +0000

    fix misformated html structure

--- www/func.php
+++ www/func.php
@@ -25,7 +25,6 @@ header("Content-type: text/html; charset=\"$charset\"");
 print('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-</html>
   <head>
     <title>QSOS.org</title>
     <meta http-equiv="Content-type" content="text/html"; charset="'.$charset.'"/>
