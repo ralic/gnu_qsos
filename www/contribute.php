656fae35424255e86a9775ba3b2c27f74c76e8b2
Raphaël Semeteys <raphael@semeteys.org>
2006-07-05 16:33:01 +0000

    Link to sheet howto (FR only)

--- www/contribute.php
+++ www/contribute.php
@@ -14,7 +14,8 @@ if ($lang=="en") {
 ?>
 	<h1>Comment contribuer</h1>
 	<ul>
-		<li><a href="docs/howto/fr/QSOS-HOWTO_grille_fonctionnelle.php">HOWTO Comment créer une nouvelle grille fonctionnelle ?</a></li>
+		<li><a href="docs/howto/fr/QSOS-HOWTO_grille_fonctionnelle.php">Comment créer une nouvelle grille fonctionnelle ?</a></li>
+		<li><a href="docs/howto/fr/QSOS-HOWTO_fiche_evaluation.php">Comment créer une nouvelle évaluation QSOS ?</a></li>
 	</ul>
 <?php
 }
