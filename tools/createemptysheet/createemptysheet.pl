83e447c3e9bc069de647b18123517d7179c7657c
Goneri Le Bouder <goneri@rulezlan.org>
2006-11-10 22:35:44 +0000

    a tool to create empty QSOS sheet from .inc and .qtpl files

---
+++ tools/createemptysheet/createemptysheet.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Getopt::Long;
+
+my $includedir;
+my $qtpl;
+
+GetOptions (
+  'include=s' => \$includedir,
+  'qtpl=s' => \$qtpl,
+);
+
+sub usage {
+  my $msg = shift;
+  print STDERR $msg."\n" if $msg;
+
+  print STDERR "usage:\n";
+  print STDERR "  createemptysheet --include=/includepath --qtpl qsos-template.qtpl:\n";
+  exit 1;
+}
+
+usage("Missing argument") if ! (defined $includedir && defined $qtpl);
+usage("Can't find include directory") if ! -d $includedir;
+usage("Can't find template file") if ! -f $qtpl;
+
+my @buff;
+open QTPL,"<$qtpl" or die "Failed to open $qtpl: $?";
+@buff = <QTPL>;
+close QTPL;
+
+while (my $line = shift @buff) {
+  if ($line =~ /<include\W+section="(\w+)"\/>/) { # this is an include
+#    print STDERR "Including $1\n";
+    open INCLUDE, "<$includedir/$1.qin" or die "Failed to open includefile
+    $1.qin";
+    unshift @buff, <INCLUDE>;
+    close INCLUDE;
+  } else {
+    print $line;
+  }
+}
