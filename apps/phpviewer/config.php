f009f50c8f20a478adcbee528c2295ecfe7f0321
Raphaël Semeteys <raphael@semeteys.org>
2006-12-11 22:49:02 +0000

    Separation between local and web paths to QSOS sheets Addition of ocal and web paths to QSOS templates

--- apps/phpviewer/config.php
+++ apps/phpviewer/config.php
@@ -1,5 +1,8 @@
 <?php
-$sheet = "../../sheet";
+$sheet = "sheets";
+$sheet_web = "http://localhost/sheets";
+$template = "template";
+$template_web = "http://localhost/template";
 $delim = "/";
 $jpgraph_path = "jpgraph-2.1.3/src/"
 ?>
