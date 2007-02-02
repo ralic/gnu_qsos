26b8e744861555a20162f092ef8ba9bcf9592d61
Raphaël Semeteys <raphael@semeteys.org>
2007-02-02 07:20:58 +0000

    phpviewer is now officially named o3s

--- apps/phpviewer/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sheet = "sheets";
-$sheet_web = "http://localhost/sheets";
-$template = "template";
-$template_web = "http://localhost/template";
-$delim = "/";
-$jpgraph_path = "jpgraph-2.1.3/src/"
-?>
--- apps/phpviewer/writeremote.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = $_FILES['myfile'];
-$destination = "incoming/".$file['name']. "." . uniqid().".qsos";
-
-move_uploaded_file($file['tmp_name'], $destination);
-chmod ($destination, 0770);
-echo "File successfully uploaded";
-?>
