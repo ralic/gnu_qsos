286e44520537e913592d2132d97661ed883ca0b1
Raphaël Semeteys <raphael@semeteys.org>
2007-02-02 07:17:04 +0000

    French locale

---
+++ apps/o3s/locales/fr-FR.php
@@ -0,0 +1,73 @@
+ <?php
+/*
+**  Copyright (C) 2007 Atos Origin 
+**
+**  Author: Raphael Semeteys <raphael.semeteys@atosorigin.com>
+**
+**  This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**
+**
+** O3S
+** fr-FR locale
+**
+*/
+
+//General
+$msg['g_license_notice'] = "Le contenu de cette page est soumis à la  <a href='http://www.gnu.org/copyleft/fdl.html'>GNU Free Documentation License</a>";
+
+//Step 1 (index.php)
+$msg['s1_title'] = "Etape 1 - Sélectionner un domaine logiciel";
+$msg['s1_table_title'] = "Domaines logiciels";
+$msg['s1_search'] = "ou effectuer une recherche par mots-clés";
+$msg['s1_button'] = "Chercher";
+//Step 1 (search.php)
+$msg['s1_button_search'] = "Chercher";
+$msg['s1_button_back'] = "Retour";
+$msg['s1_search_msg1'] = "Désolé. La recherche sur ";
+$msg['s1_search_msg2'] = " n'a retourné aucun résultat.";
+
+//Step 2 (set_weighting.php)
+$msg['s2_loaded'] = " chargé.";
+$msg['s2_error1'] = " et ";
+$msg['s2_error2'] = " ne correspondent pas.";
+$msg['s2_title'] = "Etape 2 - Entrer vos pondérations";
+$msg['s2_button_back'] = "< Etape 1";
+$msg['s2_button_save'] = "Sauvegarder";
+$msg['s2_button_next'] = "Etape 3 >";
+$msg['s2_button_upload'] = "Charger";
+$msg['s2_weight'] = "Poids";
+
+//Step 3 (software.php)
+$msg['s3_title'] = "Etape 3 - Sélectionner le(s) logiciel(s)";
+$msg['s3_button_back'] = "< Etape 2";
+$msg['s3_format_xml'] = "Fichier QSOS XML";
+$msg['s3_format_ods'] = "OpenDocument";
+$msg['s3_button_next'] = "Etape 4 - Visualiser";
+$msg['s3_check_svg'] = "Mon navigateur supporte SVG";
+
+//Step 4 (show.php)
+$msg['s4_title'] = "Etape 4 - Visualiser";
+$msg['s4_button_back'] = "< Etape 3";
+$msg['s4_button_back_alt'] = "Retour";
+$msg['s4_comments'] = "Commentaires";
+$msg['s4_score'] = "Note";
+$msg['s4_weight'] = "Poids";
+
+//Step 5 (radar.php)
+$msg['s5_error'] = "Aucun fichier QSOS n'est fourni !";
+$msg['s5_back'] = "Retour";
+$msg['s5_up'] = "Remonter";
+
+?>
