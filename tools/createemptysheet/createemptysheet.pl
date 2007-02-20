10f233e1db06de0b63ad152587816987d52f28fe
Goneri Le Bouder <goneri@rulezlan.org>
2007-02-20 20:14:28 +0000

    use of qsosappname

--- tools/createemptysheet/createemptysheet.pl
+++ tools/createemptysheet/createemptysheet.pl
@@ -4,6 +4,31 @@ use strict;
 
 use Getopt::Long;
 
+sub getLanguage {
+  my $file = shift;
+
+  my $filenamelang = 'en';
+  my $insheetlang = '';
+
+  if ($file =~ /_(\S\S)\.qtpl/) {
+    $filenamelang = $1;
+  }
+
+  open FILE,"<$file" or die "Can't open $file: $!";
+  foreach (<FILE>) {
+    if (/<language>(\S+)<\/language>/) {
+      $insheetlang = $1;
+    }
+  }
+  close $file;
+
+  if ($filenamelang ne $insheetlang) {
+    die "sheet name doesn't mach the language set in the sheet <language>$insheetlang</language>";
+  }
+
+  return $insheetlang;
+}
+
 my $includedir = "../../sheet/include/";
 my $qtpl;
 
@@ -25,7 +50,7 @@ sub usage {
 usage("Missing argument") if ! (defined $includedir && defined $qtpl);
 usage("Can't find include directory") if ! -d $includedir;
 usage("Can't find template file") if ! -f $qtpl;
-
+my $lang = getLanguage($qtpl);
 my @buff;
 open QTPL,"<$qtpl" or die "Failed to open $qtpl: $?";
 @buff = <QTPL>;
@@ -33,9 +58,12 @@ close QTPL;
 
 while (my $line = shift @buff) {
   if ($line =~ /<include\W+section="([-\w]+)"\W*(|\/)>/) { # this is an include
-#    print STDERR "Including $1\n";
-    open INCLUDE, "<$includedir/$1.qin" or die "Failed to open includefile
-    $1.qin";
+    if ($lang ne 'en' && -f "$includedir/$1_$lang.qin" ) {
+      open INCLUDE, "<$includedir/$1_$lang.qin" or die "Failed to open ".
+      "localised includefile"
+    } else {
+      open INCLUDE, "<$includedir/$1.qin" or die "Failed to open includefile"
+    }
     unshift @buff, <INCLUDE>;
     close INCLUDE;
   } else {
