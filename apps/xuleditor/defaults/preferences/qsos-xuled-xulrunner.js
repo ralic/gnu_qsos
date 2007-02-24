3685f9a9db9f888f65e5652d8f597ae5745f8dbf
Raphaël Semeteys <raphael@semeteys.org>
2007-02-24 23:04:58 +0000

    Chat preferences (chatroom and nickname)

--- apps/xuleditor/defaults/preferences/qsos-xuled-xulrunner.js
+++ apps/xuleditor/defaults/preferences/qsos-xuled-xulrunner.js
@@ -2,3 +2,5 @@ pref("toolkit.defaultChromeURI", "chrome://qsos-xuled/content/editor.xul");
 pref("extensions.qsos-xuled.loadremote-tpl", "http://www.qsos.org/o3s/loadremote.php?tpl=yes");
 pref("extensions.qsos-xuled.loadremote", "http://www.qsos.org/o3s/loadremote.php");
 pref("extensions.qsos-xuled.saveremote", "http://www.qsos.org/o3s/writeremote.php");
+pref("extensions.qsos-xuled.chatroom", "qsos@chat.jabberfr.org");
+pref("extensions.qsos-xuled.nick", "_");
--- apps/xuleditor/defaults/preferences/qsos-xuled.js
+++ apps/xuleditor/defaults/preferences/qsos-xuled.js
@@ -3,3 +3,4 @@ pref("extensions.qsos-xuled.loadremote-tpl", "http://www.qsos.org/o3s/loadremote
 pref("extensions.qsos-xuled.loadremote", "http://www.qsos.org/o3s/loadremote.php");
 pref("extensions.qsos-xuled.saveremote", "http://www.qsos.org/o3s/writeremote.php");
 pref("extensions.qsos-xuled.chatroom", "qsos@chat.jabberfr.org");
+pref("extensions.qsos-xuled.nick", "_");
