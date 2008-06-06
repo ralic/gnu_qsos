ffcf8437301e286223e41cf27ef0f55eab1848ad
Hery Randriamanamihaga <hery.randriamanamihaga@atosorigin.com>
2008-06-06 12:07:03 +0200

    Can browse until leaves are reached

--- src/Services/browse.py
+++ src/Services/browse.py
@@ -7,7 +7,7 @@ from nevow               import inevow
 
 import os
 
-class RepositoryBrowser ( rend.Page ):
+class Page404 ( rend.Page ):
 
     def render_title ( self, ctx, data ):
         request = inevow.IRequest ( ctx )
@@ -15,29 +15,79 @@ class RepositoryBrowser ( rend.Page ):
 
     def render_body ( self, ctx, data ):
         request = inevow.IRequest ( ctx )
-        path = "/".join(request.prepath[1:])
-        return path
+        return "Not Found"
 
     docFactory = loaders.stan (
         T.html [ T.head [ T.title [ render_title ] ],
                  T.body [ render_body ]
         ]
-    )
+    )  
 
-    
 class ReportsPage ( rend.Page ):
 
     def locateChild ( self, ctx, segments ):
-        return ( RepositoryBrowser(), () )
+        request = inevow.IRequest ( ctx )
+        print "/".join(request.prepath[1:])
+        return ( Page404(), () )
+     
+
+class SubPage ( rend.Page ):
+    
+    def render_title ( self, ctx, data ):
+        request = inevow.IRequest ( ctx )
+        return "Currently browsing  %s" % ( "/".join(request.prepath[1:]), )
+    
+    def body ( self, ctx, data ):
+        request = inevow.IRequest ( ctx )
+        path = "/".join(request.prepath[1:])
+        body = [ T.h1 [path] ]
+        body.extend([T.p [ T.a ( href = 'repository/'+path+'/'+dir ) [ dir ] ] for dir in os.listdir("../../sheets/"+path)])
+        return body
+    
+    docFactory = loaders.stan (
+        T.html [  T.head [ T.title [ render_title ] ], 
+                  body
+                 ]
+        )
+    
+    child_evaluations = ReportsPage();
+    
+class EvaluationPage ( rend.Page ):
+    
+    def render_title ( self, ctx, data ):
+        request = inevow.IRequest ( ctx )
+        return "Currently browsing  %s" % ( "/".join(request.prepath[1:]), )
+    
+    def body ( self, ctx, data ):
+        request = inevow.IRequest ( ctx )
+        path = "/".join(request.prepath[1:])
+        body = [ T.h1 [path] ]
+        body.extend([ [ T.p [
+                             T.a ( href = "/".join(["repository",dir,version]) ) [
+                                                               dir + "-" + version
+                                                               ]
+                             ] for version in os.listdir("../../sheets/"+path+'/'+dir)]
+                      for dir in os.listdir("../../sheets/"+path)])
+        return body
+    
+    docFactory = loaders.stan (
+        T.html [  T.head [ T.title [ render_title ] ], 
+                  body
+                 ]
+        )
+    
+    child_evaluations = ReportsPage();
 
 class MainPage ( rend.Page ):
     body = [ T.h1 ["/"] ]
-    body.extend([T.p [ T.a ( href = "repository/reports/"+dir ) [ dir ] ]for dir in os.listdir("../../sheets")])
+    body.extend([T.p [ T.a ( href = 'repository/'+dir ) [ dir ] ]for dir in os.listdir("../../sheets")])
     docFactory = loaders.stan (
         T.html [ T.head ( title = 'Main Page' ),
                  T.body [ body ]
                  ]
         )
-         
-    child_reports = ReportsPage()
+    
+    def childFactory ( self, ctx, name ):
+        return SubPage()
 
+    children = { 'evaluations' : EvaluationPage() }
