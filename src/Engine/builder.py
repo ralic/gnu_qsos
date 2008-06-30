f2a0c21729d0af019e728379e4c9cd18aaaff29d
Hery Randriamanamihaga <hery.randriamanamihaga@atosorigin.com>
2008-06-30 11:03:18 +0200

    Splitted createSheet method, so blank sheets can be easily created

--- src/Engine/builder.py
+++ src/Engine/builder.py
@@ -28,6 +28,7 @@ def build(id, repositoryroot):
     @return
         Builded Document object of name-version's qsos evaluation
     """
+    print "Builder invoked"
     #Read the template of requested evaluation from repository
     template = os.path.join(repositoryroot,"sheets","templates", id + ".qtpl")
     template = "".join(line.strip() for line in file(template).readlines())
@@ -106,47 +107,46 @@ def assembleSheet(document, repositoryroot):
     header.appendChild(dates)
     root.appendChild(header)
     
-    #Lambda fucntions :
-    #    * add a text node in an element
-    #    * check if the element has the child node and create it if not
-    
-    addTextNode = lambda element,tag,text :                                     \
-                    element.                                                    \
-                        getElementsByTagName(tag).item(0).                      \
-                        appendChild(sheet.createTextNode(text))
-                        
-    checkChildNode = lambda element,tag :                                       \
-                    element.                                                    \
-                            getElementsByTagName(tag)                           \
-                        or                                                      \
-                            element.appendChild(sheet.createElement(tag))
-    
     #Fill in header with properties
     for item in document["properties"] :
         tag = sheet.createElement(item)
         tag.appendChild(sheet.createTextNode(document["properties"][item]))
         header.appendChild(tag)
-    families = document["families"].keys()
-    
+
     #Add blank qsos evaluation of families
+    families = document["families"].keys()
     for item in families :
-        include = ".".join([item, "qin"])
-        include = os.path.join(repositoryroot,"sheets","families",include)
-        include = file(include).read()
-        include = "".join([line.strip() for line in include.splitlines()])
+        include = os.path.join(repositoryroot,"sheets","families",item + ".qin")
+        include = "".join(line.strip() for line in file(include).readlines())
         include = minidom.parseString(include).firstChild
-        for section in include.childNodes[1:]:
-            root.appendChild(section)
+        for section in include.childNodes[1:] : root.appendChild(section)
+            
     
     #Finalize the blank  document
     sheet.appendChild(root)
+    print document["families"]
+    return sheet.toxml('utf-8')
+
+##
+#    @ingroup builder
+#
+def fillSheet(document, sheet, repositoryroot):   
+    #Lambda fucntions :
+    #    * add a text node in an element
+    #    * check if the element has the child node and create it if not
     
-            
-    sheet = minidom.parseString(sheet.toxml())
+    addTextNode = lambda element,tag,text : element.getElementsByTagName(tag).  \
+                                item(0).appendChild(sheet.createTextNode(text))
+                        
+    checkChildNode = lambda element,tag :                                       \
+                            element.getElementsByTagName(tag)                   \
+                        or                                                      \
+                            element.appendChild(sheet.createElement(tag))
+                            
+    sheet = minidom.parseString(sheet)
     
     #Define ID tag for the document
-    for elements in sheet.getElementsByTagName("element") :
-        elements.setIdAttribute("name")
+    for e in sheet.getElementsByTagName("element") : e.setIdAttribute("name")
     
     #Fill-in evaluations' section with families data
     for item in document["families"] :
