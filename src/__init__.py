0edce0bb57d79ceb308f587a4eb833b98ed3a30d
Hery Randriamanamihaga <hery.randriamanamihaga@atosorigin.com>
2008-06-30 12:34:18 +0200

    Installation Guide into doxygen documentation

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,26 @@
 #This is the new implementation of QSOS. Even though this version provides minimalistic
 #features of final implementation, the whole submit and request processes are implemented
 #
+#@section installation Installation Guide
+#@subsection configuration Workspace Configuration
+#- Python : install it using the distrib package manager
+#    (for debian, apt-get install python)
+#- Twisted  (twisted is required to run nevow web application framework) :
+#    - Use distrib package manager twisted
+#- Nevow (web application framework)
+#    - Download sources from Nevow wiki : http://divmod.org/trac/attachment/wiki/SoftwareReleases/Nevow-0.9.31.tar.gz?format=raw
+#    - Uncompress tarball
+#    - Run installation script from uncompressed tarball: python setup.py install
+#- Doxygen (for automatic documentation generation)
+#    - Install using distrib package manager : apt-get install doxygen
+#@subsection run Run the application
+#- Clone the git repository :
+#    - git clone git://git.savannah.nongnu.org/qsos.git
+#- Go into src dir
+#    - cd qsos/src
+#- Run presentation twisted application
+#    - twistd -noy Services/presentation.tac
+
 #@see
 #    - http://www.qsos.org/
 #    - https://savannah.nongnu.org/projects/qsos/
