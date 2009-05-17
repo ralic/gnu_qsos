25119c29b25d5909d7ae4e9dba9d21a9972df090
Raphaël Semeteys <raphael@semeteys.org>
2009-05-17 22:20:45 +0000

    Database integration

--- apps/o3s/set_weighting.php
+++ apps/o3s/set_weighting.php
@@ -1,6 +1,6 @@
 <?php
 /*
-**  Copyright (C) 2007 Atos Origin 
+**  Copyright (C) 2007-2009 Atos Origin 
 **
 **  Author: Raphael Semeteys <raphael.semeteys@atosorigin.com>
 **
@@ -23,15 +23,36 @@
 ** set_weighting.php: displays form to enter weigthings
 **
 */
-
 session_start();
-session_unset();
-session_destroy();
-$_SESSION = array();
+
+$params = array('lang', 'svg', 'family', 'qsosspecificformat', 'new');
+
+foreach($params as $param) {
+  $$param = $_REQUEST[$param];
+}
+
+$weights = array();
+
+if (isset($new) && ($new == "true")) {
+  $_SESSION = array();
+  while (list($name, $value) = each($_REQUEST)) { 
+    if (!(in_array($name,$params))) {
+      $_SESSION[$name] = $value;
+    }
+  }
+}
+
+while (list($name, $value) = each($_SESSION)) { 
+  if (!(in_array($name,$params))) {
+    $weights[$name] = $_SESSION[$name];
+  }
+}
+
+
+$_SESSION["nbWeights"] = count($weights);
 
 include("config.php");
-include("fs.functions.php");
-include("locales/$lang.php");
+include("lang.php");
 
 echo "<html>\n";
 echo "<head>\n";
@@ -41,32 +62,37 @@ echo "<LINK REL=StyleSheet HREF='skins/$skin/o3s.css' TYPE='text/css'/>\n";
 <script language="JavaScript" type="text/javascript">
 
 function checkWeight(field) {
-	if (isNaN(field.value)) {
-		var oldValue = field.value;
-		field.value = "1";
-		alert(oldValue + " is not an applicable weight value,\n it has been set to 1.");
-		field.focus();
-	}
+  if (isNaN(field.value)) {
+    var oldValue = field.value;
+    field.value = "1";
+    alert(oldValue + "<? echo $msg['s2_err_weight']; ?>");
+    field.focus();
+  }
+}
+
+function saveFile() {
+  myForm.action = "save_weighting.php";
+  myForm.submit();
 }
 
 function save() {
-	myForm.action = "save_weighting.php";
-	myForm.submit();
+  myForm.action = "set_weighting.php";
+  myForm.submit();
 }
 
-function next() {
-	myForm.action = "software.php";
-	myForm.submit();
+function back() {
+  myForm.action = "list.php";
+  myForm.submit();
 }
 
 function upload() {
-	var file = document.getElementById("weighting");
-	if (file.value == "") {
-		alert("No weight file is provided!");
-		return;
-	}
-	myForm.action = "set_weighting.php";
-	myForm.submit();
+  var file = document.getElementById("weighting");
+  if (file.value == "") {
+    alert("<? echo $msg['s2_err_no_file']; ?>");
+    return;
+  }
+  myForm.action = "set_weighting.php";
+  myForm.submit();
 }
 </script>
 <?php
@@ -79,140 +105,135 @@ echo "<br/><br/>\n";
 
 include("libs/QSOSDocument.php");
 
-$family = $_REQUEST['family'];
-$svg = $_REQUEST['svg'];
+//Check if family and template version exist
+$IdDB = mysql_connect($db_host ,$db_user, $db_pwd);
+mysql_select_db($db_db);
+$query = "SELECT DISTINCT CONCAT(qsosappfamily,qsosspecificformat) FROM evaluations WHERE appname <> '' AND language = '$lang'";
+$IdReq = mysql_query($query, $IdDB);
+$familiesFQDN = array();
+while($row = mysql_fetch_row($IdReq)) {
+  array_push($familiesFQDN, $row[0]);
+}
+if (!in_array($family.$qsosspecificformat,$familiesFQDN)) 
+  die ("$family $qsosspecificformat".$msg['s3_err_no_family']);
 
-$upload = false;
-$weights = array();
+$query = "SELECT file FROM evaluations WHERE qsosappfamily = '$family' AND qsosspecificformat = '$qsosspecificformat' LIMIT 0,1";
+$IdReq = mysql_query($query, $IdDB);
+$result = mysql_fetch_row($IdReq);
+$file = $result[0];
 
 //Upload of weighting file
-if (isset($_FILES['weighting'])) {
-	$weighting = $_FILES['weighting'];
-	$dir = $temp.uniqid();
-	move_uploaded_file($weighting['tmp_name'], $dir);
-	chmod ($dir, 0770);
+if (isset($_FILES['weighting']) && $_FILES['weighting']['tmp_name'] <> "") {
+  $weighting = $_FILES['weighting'];
+  $dir = $temp.uniqid();
+  move_uploaded_file($weighting['tmp_name'], $dir);
+  chmod ($dir, 0770);
 
-	$doc = new DOMDocument();
-	$doc->load($dir);
-	$xpath =  new DOMXPath($doc);
+  $doc = new DOMDocument();
+  $doc->load($dir);
+  $xpath =  new DOMXPath($doc);
 
-	$nodes = $xpath->query("//family");
-	$upload_family = $nodes->item(0)->nodeValue;
+  $nodes = $xpath->query("//family");
+  $upload_family = $nodes->item(0)->nodeValue;
 
-	if ($upload_family == $family) {
-		$nodes = $xpath->query("//weight");
-		foreach ($nodes as $node) {
-			$name = $node->getAttribute('id');
-			$value = $node->nodeValue;
-			$weights[$name] = $value;
-		}
-		$text = "<div style='texte-align: center; color: red'>"
-			.$weighting['name']
-			.$msg['s2_loaded']
-			."</div><br/>";
-		$upload = true;
-	} else {
-		$text = "<div style='texte-align: center; color: red'>"
-			.$family.$msg['s2_error1']
-			.$upload_family.$msg['s2_error2']
-			."</div><br/>";
-	}
+  if ($upload_family == $family) {
+    $nodes = $xpath->query("//weight");
+    foreach ($nodes as $node) {
+      $name = $node->getAttribute('id');
+      $value = $node->nodeValue;
+      $weights[$name] = $value;
+    }
+    $text = "<div style='texte-align: center; color: red'>"
+      .$weighting['name']
+      .$msg['s2_loaded']
+      ."</div><br/>";
+  } else {
+    $text = "<div style='texte-align: center; color: red'>"
+      .$family.$msg['s2_error1']
+      .$upload_family.$msg['s2_error2']
+      ."</div><br/>";
+  }
 }
-
-$tree= retrieveLocalizedTree($sheet.$delim.$family, $locale);
-$keys = array_keys($tree);
-
-$file = $tree[$keys[0]][0];
-$file = $sheet.$delim.$family.$delim.$keys[0].$delim.$file;
 
 $myDoc = new QSOSDocument($file);
 $tree = $myDoc->getTree();
 $familyname = $myDoc->getkey("qsosappfamily");
 
-echo "<div style='font-weight: bold'>"
-	.$msg['s2_title']
-	."<br/><br/></div>\n";
+echo "<div style='font-weight: bold'>".$msg['s2_title']."<br/><br/></div>\n";
 echo $text;
-echo "<form id='myForm' 
-	enctype='multipart/form-data' 
-	method='POST' 
-	action='software.php'>\n";
-echo "<input type='hidden' 
-	name='family' 
-	value='$family'/>\n";
-echo "<table id='table' 
-	style='border-collapse: collapse; font-size: 12pt; table-layout: fixed'>\n";
+echo "<form id='myForm' enctype='multipart/form-data' method='POST' action='set_weighting.php'>\n";
+echo "<input type='hidden' name='lang' value='$lang'/>\n";
+echo "<input type='hidden' name='svg' value='$svg'/>\n";
+echo "<input type='hidden' name='family' value='$family'/>\n";
+echo "<input type='hidden' name='new' value='true'/>\n";
+echo "<input type='hidden' name='qsosspecificformat' value='$qsosspecificformat'/>\n";
+echo "<table id='table' style='border-collapse: collapse; font-size: 12pt; table-layout: fixed'>\n";
 echo "<tr class='title' style='width: 400px'><td>$familyname</td>\n";
 echo "<td><div style='width: 60px; text-align: center'>"
-	.$msg['s2_weight']
-	."</div></td>\n";
+  .$msg['s2_weight']
+  ."</div></td>\n";
 echo "</tr>\n";
 
 showtree($myDoc, $tree, 0, '');
 
-echo "<input type='button' 
-	value='".$msg['s2_button_back']."' 
-	onclick='location.href=\"index.php\"'> ";
-echo " <input type='button' 
-	value='".$msg['s2_button_save']."' 
-	onclick='save()'> ";
-echo " <input type='button' 
-	value='".$msg['s2_button_next']."' 
-	onclick='next()'><br/><br/>\n";
-echo "<input type='file' 
-	id='weighting' name='weighting'> 
-      <input type='button' 
-	value='".$msg['s2_button_upload']."' 
-	onclick='upload()'><br/><br/>\n";
+echo "<input type='button' value='".$msg['s2_button_back']."' 
+  onclick='back()'> ";
+echo " <input type='button' value='".$msg['s2_button_save']."' 
+  onclick='save()'> ";
+echo " <input type='button' value='".$msg['s2_button_saveFile']."' 
+  onclick='saveFile()'><br/><br/>\n";
+echo "<input type='file' id='weighting' name='weighting'/> ";
+echo "<input type='button' value='".$msg['s2_button_upload']."' 
+  onclick='upload()'><br/><br/>\n";
 echo "</table>\n";
 echo "</form>\n";
 
 //Recursive function to display tree of criteria
 function showtree($myDoc, $tree, $depth, $idP) {
-	global $upload;
-	global $weights;
-	$new_depth = $depth + 1;
-	$offset = $new_depth*10;
-	$idF = 0;
+  global $upload;
+  global $weights;
+  $new_depth = $depth + 1;
+  $offset = $new_depth*10;
+  $idF = 0;
 
-	for($k=0; $k<count($tree); $k++) {
-		$name = $tree[$k]->name;
-		$title = $tree[$k]->title;
-		$subtree = $tree[$k]->children;
-	
-		$idF++;
-		if ($idP == '') {
-			$id = $idF;
-		} else  {
-			$id = $idP."-".$idF;
-		}
-	
-		echo "<tr id='$id' 
-			class='level$depth' 
-			onmouseover=\"this.setAttribute('class','highlight')\" 
-			onmouseout=\"this.setAttribute('class','level$depth')\">\n";
-		if ($subtree) {
-			echo "<td style='width: 400px; text-indent: $offset'>
-				<span onclick=\"collapse(this);\" class='expanded'>$title</span></td>\n";
-		} else {
-			echo "<td style='width: 400px; text-indent: $offset'>
-				<span>$title</span></td>\n";
-		}
-	
-		echo "<td><div style='width: 60px; text-align: center'>\n";
-		//If a weighting file has been uploaded use $weights array, if not use default value 1
-		echo "<input type='text' 
-			name='$name' size='3' style='text-align: center' onblur='checkWeight(this)' 
-			value='".(($upload)?$weights[$name]:1)."'/>\n";
+  for($k=0; $k<count($tree); $k++) {
+    $name = $tree[$k]->name;
+    $title = $tree[$k]->title;
+    $subtree = $tree[$k]->children;
+  
+    $idF++;
+    if ($idP == '') {
+      $id = $idF;
+    } else  {
+      $id = $idP."-".$idF;
+    }
+  
+    echo "<tr id='$id' 
+      class='level$depth' 
+      onmouseover=\"this.setAttribute('class','highlight')\" 
+      onmouseout=\"this.setAttribute('class','level$depth')\">\n";
+    if ($subtree) {
+      echo "<td style='width: 400px; text-indent: $offset'>
+        <span onclick=\"collapse(this);\" class='expanded'>$title</span></td>\n";
+    } else {
+      echo "<td style='width: 400px; text-indent: $offset'>
+        <span>$title</span></td>\n";
+    }
+  
+    echo "<td><div style='width: 60px; text-align: center'>\n";
+    //If a weighting file has been uploaded use $weights array, if not use default value 1
+    echo "<input type='text' 
+      name='$name' size='3' style='text-align: center' onblur='checkWeight(this)' 
+      value='".((isset($weights[$name]))?$weights[$name]:1)."'/>\n";
 
-		echo "</div></td>\n";
-	
-		echo "</tr>\n";
+    echo "</div></td>\n";
+  
+    echo "</tr>\n";
 
-		if ($subtree) {
-			showtree($myDoc, $subtree, $new_depth, $id);
-		}
-	}
+    if ($subtree) {
+      showtree($myDoc, $subtree, $new_depth, $id);
+    }
+  }
 }
 
 echo "</center>\n";
--- apps/o3s/show.php
+++ apps/o3s/show.php
@@ -1,6 +1,6 @@
 <?php
 /*
-**  Copyright (C) 2007 Atos Origin 
+**  Copyright (C) 2007-2009 Atos Origin 
 **
 **  Author: Raphael Semeteys <raphael.semeteys@atosorigin.com>
 **
@@ -23,15 +23,16 @@
 ** show.php: show QSOS evaluation(s)
 **
 */
-
 session_start();
+
+$weights = $_SESSION;
+$is_weighted = $_SESSION["nbWeights"];
+
 //Search pattern
 $s = $_REQUEST['s'];
-//If there is a search pattern, no weights are to be applied
-$is_weighted = !isset($s);
 
 include("config.php");
-include("locales/$lang.php");
+include("lang.php");
 
 echo "<html>\n";
 echo "<head>\n";
@@ -43,43 +44,43 @@ echo "<LINK REL=StyleSheet HREF='skins/$skin/o3s.css' TYPE='text/css'/>\n";
 var size = 12;
 
 function showComments() {
-	var cells = document.getElementsByTagName("td");
-	for (var i = 0; i < cells.length; i++) {
-		var c = cells[i];
-		if (c.id == 'comment') {
-			if (document.all) c.style.display = "block"; //IE4+ specific code
-    			else c.style.display = "table-row"; //Netscape and Mozilla
-		}
-	}
-	document.getElementById("comment_selector").href = "javascript:hideComments();";
-	document.getElementById("column").src = "<?php echo "skins/$skin/hide-comments.png"; ?>";
+  var cells = document.getElementsByTagName("td");
+  for (var i = 0; i < cells.length; i++) {
+    var c = cells[i];
+    if (c.id == 'comment') {
+      if (document.all) c.style.display = "block"; //IE4+ specific code
+          else c.style.display = "table-row"; //Netscape and Mozilla
+    }
+  }
+  document.getElementById("comment_selector").href = "javascript:hideComments();";
+  document.getElementById("column").src = "<?php echo "skins/$skin/hide-comments.png"; ?>";
 }
 
 function hideComments() {
-	var cells = document.getElementsByTagName("td");
-	for (var i = 0; i < cells.length; i++) {
-		var c = cells[i];
-		if (c.id == 'comment') {
-			c.style.display = "none";
-		}
-	}
-	document.getElementById("comment_selector").href = "javascript:showComments();";
-	document.getElementById("column").src = "<?php echo "skins/$skin/show-comments.png"; ?>";
+  var cells = document.getElementsByTagName("td");
+  for (var i = 0; i < cells.length; i++) {
+    var c = cells[i];
+    if (c.id == 'comment') {
+      c.style.display = "none";
+    }
+  }
+  document.getElementById("comment_selector").href = "javascript:showComments();";
+  document.getElementById("column").src = "<?php echo "skins/$skin/show-comments.png"; ?>";
 }
 
 function decreaseFontSize() {
-	size--;
-	document.getElementById("table").style.fontSize = size + "pt";
+  size--;
+  document.getElementById("table").style.fontSize = size + "pt";
 }
 
 function increaseFontSize() {
-	size++;
-	document.getElementById("table").style.fontSize = size + "pt";
+  size++;
+  document.getElementById("table").style.fontSize = size + "pt";
 }
 
 function submitForm(c) {
-	document.getElementById("c").value = c;
-	myForm.submit();
+  document.getElementById("c").value = c;
+  myForm.submit();
 }
 </script>
 <?php
@@ -87,34 +88,48 @@ echo "</head>\n";
 
 include("libs/QSOSDocument.php");
 
-//Software family
-$family = $_REQUEST['family'];
-//QSOS XML files to be displayed
-$files = $_REQUEST['f'];
+//Ids of QSOS XML files to be displayed
+$ids = $_REQUEST['id'];
 //Are graphs to be generated in SVG?
 $svg = $_REQUEST['svg'];
 
+function janitize($text) {
+  return str_replace("\n",'',$text);
+}
+
 if (isset($s)) {
-	echo "<body onload=\"highlightSearchTerms('$s');\">\n";
+  echo "<body onload=\"highlightSearchTerms('$s');\">\n";
 } else {
-	echo "<body>\n";
+  echo "<body>\n";
 }
 echo "<center>\n";
 echo "<img src='skins/$skin/o3s.png'/>\n";
 echo "<br/><br/>\n";
 echo "<div style='font-weight: bold'>".$msg['s4_title']."<br/><br/>\n";
 
-echo "<form id='myForm' method='POST' action='radar.php'>\n";
+$IdDB = mysql_connect($db_host ,$db_user, $db_pwd);
+mysql_select_db($db_db);
 
-$weights = $_SESSION;
-
-foreach($files as $file) {
-	echo "<input type='hidden' name='f[]' value='$file'/>\n";
+$query = "SELECT id FROM evaluations WHERE appname <> '' AND language = '$lang'";
+$IdReq = mysql_query($query, $IdDB);
+$allIds = array();
+while($row = mysql_fetch_row($IdReq)) {
+  array_push($allIds, $row[0]);
 }
-echo "<input type='hidden' name='svg' value='$svg'/>\n";
-echo "<input type='hidden' name='c' id='c' value=''/>\n";
-echo "<input type='hidden' name='family' value='$family'/>\n";
-echo "<input type='hidden' name='s' value='$s'/>\n";
+
+$files = array();
+foreach($ids as $id) {
+  if (!(in_array($id,$allIds))) die($id.$msg['s4_err_no_id']);
+  $query = "SELECT file FROM evaluations WHERE id = \"$id\"";
+  $IdReq = mysql_query($query, $IdDB);
+  $result = mysql_fetch_row($IdReq);
+  array_push($files, $result[0]);
+}
+
+//echo "<form id='myForm' method='POST' action='radar.php'>\n";
+//echo "<input type='hidden' name='svg' value='$svg'/>\n";
+//echo "<input type='hidden' name='c' id='c' value=''/>\n";
+//echo "<input type='hidden' name='s' value='$s'/>\n";
 
 $num = count($files);
 $myDoc = array();
@@ -123,79 +138,81 @@ $trees = array();
 
 $i = 0;
 foreach($files as $file) {
-	$myDoc[$i] = new QSOSDocument($file);
-	$app[$i] = $myDoc[$i]->getkey("appname")." ".$myDoc[$i]->getkey("release");
-	if ($is_weighted) {
-		$trees[$i] = $myDoc[$i]->getWeightedTree($weights);
-	} else {
-		$trees[$i] = $myDoc[$i]->getTree();
-	}
-	$i++;
+  $myDoc[$i] = new QSOSDocument($file);
+  $app[$i] = $myDoc[$i]->getkey("appname")." ".$myDoc[$i]->getkey("release");
+  if ($is_weighted) {
+    $trees[$i] = $myDoc[$i]->getWeightedTree($weights);
+  } else {
+    $trees[$i] = $myDoc[$i]->getTree();
+  }
+  $i++;
 }
 
-$familyname = $myDoc[0]->getkey("qsosappfamily");
+$family = $myDoc[0]->getkey("qsosappfamily");
+$qsosspecificformat = $myDoc[0]->getkey("qsosspecificformat");
 
 $f = "";
-foreach($files as $file) {
-	$f .= "f[]=$file&";
+foreach($ids as $id) {
+  $f .= "id[]=$id&";
 }
 
 echo "<table>\n";
 echo "<tr width='100%' align='center'><td>\n";
 echo "<a id='comment_selector' href='javascript:hideComments();'>";
 echo "<img id='column' 
-	src='skins/$skin/hide-comments.png' 
-	border=0 
-	onmouseover=\"return escape('Hide/Show comments')\"/>";
+  src='skins/$skin/hide-comments.png' 
+  border=0 
+  onmouseover=\"return escape('Hide/Show comments')\"/>";
 echo "</a>\n";
 echo " <a href='javascript:decreaseFontSize();'>";
 echo "<img src='skins/$skin/decrease-font.png' 
-	border=0 
-	onmouseover=\"return escape('Decrease font size')\"/>";
+  border=0 
+  onmouseover=\"return escape('Decrease font size')\"/>";
 echo "</a>\n";
 echo " <a href='javascript:increaseFontSize();'>";
 echo "<img src='skins/$skin/increase-font.png' 
-	border=0 
-	onmouseover=\"return escape('Increase font size')\"/>";
+  border=0 
+  onmouseover=\"return escape('Increase font size')\"/>";
 echo "</a>\n";
-if ($is_weighted) {
-	echo " <a href='radar.php?family=$family&".$f."svg=$svg'>";
-	echo "<img src='skins/$skin/graph.png' 
-		border=0 onmouseover=\"return escape('Show graph')\"/>";
-	echo "</a>\n";
+if (!isset($s)) {
+  echo " <a href='radar.php?lang=$lang&".$f."svg=$svg'>";
+  echo "<img src='skins/$skin/graph.png' 
+    border=0 onmouseover=\"return escape('Show graph')\"/>";
+  echo "</a>\n";
 }
 echo "</td></tr>\n";
 echo "<tr><td align='center'>\n";
-if ($is_weighted) {
-	echo "<input type='button' 
-		value='".$msg['s4_button_back']
-		."' onclick='location.href=\"software.php?family=".$family."&svg=$svg\"'><br/><br/>";
+if (isset($s)) {
+  echo "<input type='button' 
+    value='".$msg['s4_button_back_alt']
+    ."' onclick='location.href=\"search.php?s=$s\"'><br/><br/>";
 } else {
-	echo "<input type='button' 
-		value='".$msg['s4_button_back_alt']
-		."' onclick='location.href=\"search.php?s=$s\"'><br/><br/>";
+  echo "<input type='button' 
+    value='".$msg['s4_button_back']
+    ."' onclick='location.href=\"list.php?lang=$lang&family=".$family."&qsosspecificformat=$qsosspecificformat&svg=$svg\"'><br/><br/>";
 }
+
 echo "</td></tr>\n";
 echo "</table>\n";
 
-echo "<table id='table' style='border-collapse: collapse; font-size: 12pt; table-layout: fixed'>\n";
+echo "<table id='table' style='font-size: 12pt; table-layout: fixed'>\n";
 echo "<tr class='title' style='width: 250px'>\n";
-echo "<td rowspan='2'><div style='text-align: center'>$familyname</div></td>\n";
+echo "<td rowspan='2'><div style='text-align: center'>$family</div></td>\n";
 echo "<td style='width: 30px' rowspan='2'>";
 if ($is_weighted) {
-	echo "<img src='skins/$skin/graph.png' border='' style='cursor: pointer' onclick='submitForm(\"\")'/>";
+  echo "<img src='skins/$skin/graph.png' border='' style='cursor: pointer' onclick='submitForm(\"\")'/>";
 }
 echo "</td>\n";
 for($i=0; $i<$num; $i++) {
-	echo "<td colspan='2'><div style='width: 120px; text-align: center'>$app[$i]</div></td>\n";
-	echo "<td id='comment' style='width: 300px'>".$msg['s4_comments']."</td>\n";
+  echo "<td colspan='2'><div style='width: 120px; text-align: center'>$app[$i]</div></td>\n";
+  echo "<td id='comment' style='width: 300px'>".$msg['s4_comments']."</td>\n";
 }
 echo "</tr>\n";
 echo "<tr class='title'>\n";
 for($i=0; $i<$num; $i++) {
-	echo "<td><div style='width: 60px; text-align: center'>".$msg['s4_score']."</div></td>\n";
-	echo "<td><div style='width: 60px; text-align: center'>".$msg['s4_weight']."</div></td>\n";
-	echo "<td id='comment' style='width: 300px'></td>\n";
+  echo "<td><div style='width: 60px; text-align: center'>".$msg['s4_score']."</div></td>\n";
+  echo "<td><div style='width: 60px; text-align: center'>".$msg['s4_weight']."</div></td>\n";
+  echo "<td id='comment' style='width: 300px'></td>\n";
 }
 echo "</tr>\n";
 
@@ -203,98 +220,93 @@ showtree($myDoc, $trees, 0, '', $weights);
 echo "</table>\n";
 
 function showtree($myDoc, $trees, $depth, $idP, $weights) {
-	global $svg;
-	global $is_weighted;
-	global $skin;
-	$new_depth = $depth + 1;
-	$offset = $new_depth*10;
-	$idF = 0;
-	$tree = $trees[0];
+  global $id, $f;
+  global $files;
+  global $svg;
+  global $is_weighted;
+  global $skin;
+  global $lang;
+  $new_depth = $depth + 1;
+  $offset = $new_depth*10;
+  $idF = 0;
+  $tree = $trees[0];
 
-	for($k=0; $k<count($tree); $k++) {
-		$name = $tree[$k]->name;
-		$title = $tree[$k]->title;
-		$subtree = $tree[$k]->children;
-		$subtrees = array();
+  for($k=0; $k<count($tree); $k++) {
+    $name = $tree[$k]->name;
+    $title = $tree[$k]->title;
+    $subtree = $tree[$k]->children;
+    $subtrees = array();
 
-		$idF++;
-		if ($idP == '') {
-			$id = $idF;
-		} else  {
-			$id = $idP."-".$idF;
-		}
+    $idF++;
+    if ($idP == '') {
+      $idDOM = $idF;
+    } else  {
+      $idDOM = $idP."-".$idF;
+    }
 
-		echo "<tr id='$id' 
-			name='$name' 
-			class='level$depth' 
-			onmouseover=\"this.setAttribute('class','highlight')\" 
-			onmouseout=\"this.setAttribute('class','level$depth')\">\n";
-		if ($subtree) {
-			echo "<td style='width: 250px; text-indent: $offset'>
-				<span onclick=\"collapse(this);\" class='expanded'>$title</span>
-				</td>\n";
-			echo "<td style='width: 30px'>";
-			if ($myDoc[0]->hassubelements($name) > 2) {
-				$files = $_REQUEST['f'];
-				$f = "";
-				foreach($files as $file) {
-					$f .= "f[]=$file&";
-				}
-				if ($is_weighted) {
-					echo "<img src='skins/$skin/graph.png' 
-						border='' 
-						style='cursor: pointer' onclick='submitForm(\"$name\")'/>\n";
-				}
-			}
-		} else {
-			echo "<td style='width: 250px; text-indent: $offset'>
-				<span>$title</span>
-				</td>\n";
-			echo "<td style='width: 30px'></td>\n";;
-		}
+    echo "<tr id='$idDOM' 
+      name='$name' 
+      class='level$depth' 
+      onmouseover=\"this.setAttribute('class','highlight')\" 
+      onmouseout=\"this.setAttribute('class','level$depth')\">\n";
+    if ($subtree) {
+      echo "<td style='width: 250px; text-indent: $offset'>
+        <span onclick=\"collapse(this);\" class='expanded'>$title</span>
+        </td>\n";
+      echo "<td style='width: 30px'>";
+      if ($myDoc[0]->hassubelements($name) > 2) {
+        if (!isset($s)) {
+          echo "<a href='radar.php?lang=$lang&".$f."c=$name&svg=$svg'><img src='skins/$skin/graph.png' border='' style='cursor: pointer'/></a>\n";
+        }
+      }
+    } else {
+      echo "<td style='width: 250px; text-indent: $offset'>
+        <span>$title</span>
+        </td>\n";
+      echo "<td style='width: 30px'></td>\n";;
+    }
 
-		for($i=0; $i<count($trees); $i++) {
-			$desc = addslashes($myDoc[$i]->getgeneric($name, "desc".$trees[$i][$k]->score));
-			if ($desc != "") {
-				echo "<td class='score' 
-					style='width: 60px; cursor:help' onmouseover=\"return escape('".$desc."')\">
-					<div style='text-align: center'>"
-						.$trees[$i][$k]->score
-					."</div></td>\n";
-			} else {
-				echo "<td class='score' 
-					style='width: 60px; text-align: center'>
-					<div style='text-align: center'>"
-						.$trees[$i][$k]->score
-					."</div></td>\n";
-			}
-			if ($is_weighted) {
-				echo "<td>
-					<div style='text-align: center'>"
-						.$weights[$name]
-					."</div></td>\n";
-			} else {
-				echo "<td>
-					<div style='text-align: center'>1</div>
-					</td>\n";
-			}
-			echo "<td id='comment'>
-					<div style='width: 300px'>"
-						.$myDoc[$i]->getgeneric($name, "comment")
-					."</div></td>\n";
-		}
-		echo "</tr>\n";
-;
-		if ($subtree) {
-			for($i=0; $i<count($trees); $i++) {
-				$subtrees[$i] = $trees[$i][$k]->children;
-			}
-			showtree($myDoc, $subtrees, $new_depth, $id, $weights);
-		}
-	}
+    for($i=0; $i<count($trees); $i++) {
+      $desc = addslashes($myDoc[$i]->getgeneric($name, "desc".$trees[$i][$k]->score));
+      if ($desc != "") {
+        echo "<td class='score' 
+          style='width: 60px; cursor:help' onmouseover=\"return escape('".janitize($desc)."')\">
+          <div style='text-align: center'>"
+            .$trees[$i][$k]->score
+          ."</div></td>\n";
+      } else {
+        echo "<td class='score' 
+          style='width: 60px; text-align: center'>
+          <div style='text-align: center'>"
+            .$trees[$i][$k]->score
+          ."</div></td>\n";
+      }
+      if (!$is_weighted) {
+        $weights[$name] = 1;
+        $_SESSION[$name] = $weights[$name];
+      }
+      echo "<td>
+        <div style='text-align: center'>"
+          .$weights[$name]
+        ."</div></td>\n";
+
+      echo "<td id='comment'>
+          <div style='width: 300px'>"
+            .$myDoc[$i]->getgeneric($name, "comment")
+          ."</div></td>\n";
+    }
+    echo "</tr>\n";
+
+    if ($subtree) {
+      for($i=0; $i<count($trees); $i++) {
+        $subtrees[$i] = $trees[$i][$k]->children;
+      }
+      showtree($myDoc, $subtrees, $new_depth, $idDOM, $weights);
+    }
+  }
 }
 
-echo "</form>";
+//echo "</form>";
 
 echo "<br/>";
 echo $msg['g_license_notice'];
